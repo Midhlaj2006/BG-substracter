c9cb37a6bdbea4206d25dd339d504b9b23ef4ca8
Midhlaj K <70678648+Midhlaj2006@users.noreply.github.com>
2020-12-07 16:15:53 +0530

    👋🏼 A small change..
    Now you can choose which frame you have to see from the execution

--- Bg-substracter.py
+++ Bg-substracter.py
@@ -8,7 +8,9 @@ f3 = cv2.bgsegm.createBackgroundSubtractorGMG()
   
 # capture frames from a camera  
 cap = cv2.VideoCapture(0)
-while(1): 
+# asking which frame you wants
+inp =int(input("choose the number of the frame you want (one at at time/all of them):\n0-all\n1-MOG\n2-MOG2\n3-GMG\n"))
+while True: 
     # read frames 
     ret, img = cap.read() 
       
@@ -16,8 +18,6 @@ while(1):
     mask1 = f1.apply(img)
     mask2 = f2.apply(img)
     mask3 = f3.apply(img)
-    #showing original clips
-    cv2.imshow('Original', img)
     #defining frames
     def MOG():
         cv2.imshow('MOG', mask1)
@@ -25,9 +25,21 @@ while(1):
         cv2.imshow('MOG2', mask2)
     def GMG():
         cv2.imshow('GMG', mask3)
-    #change this to others
-    MOG()
-       
+    #choosing the frame
+    if inp==0:
+        MOG()
+        MOG2()
+        GMG()
+    elif inp==1:
+        MOG()
+    elif inp==2:
+        MOG2()
+    elif inp==3:
+        GMG()
+    else:
+        print(f'Sorry {inp} is not found \nonly showing you your camera feed')
+    #show original video
+    cv2.imshow('Original', img)
     #running the clips
     key = cv2.waitKey(30) & 0xff 
     if key == 27: 
